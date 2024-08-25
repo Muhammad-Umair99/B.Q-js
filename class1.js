ab2e974260eea84493e9acbf97c901310b5033c8
Muhammad-Umair99 <muhammadumair10010@gmail.com>
2024-08-25 12:23:14 +0500

    basics & CONDITIONAL STATEMENT

---
+++ class1.js
@@ -0,0 +1,60 @@
+// var value1 = 55;
+
+// var value2 = 21;
+// console.log(value1)
+
+
+
+// math expression
+// add , sub, div, mul , modulus
+
+// var value1 = 7;
+// var value2 = 2;
+// var addition = value1+value2
+// var subtraction = value1-value2
+// var division = value1/value2
+// var multiplication = value1*value2
+// var modulus = value1 % value2
+// console.log(addition, multiplication, division, modulus, subtraction)
+
+// unfamilar math operation
+//  var num1 = 5;
+//  var num2 = 6;
+//  reinitialize : redeclare
+//  var num1 = 5 + 1;
+// console.log(num1);
+
+
+//  post increment
+// var num1 = num1++
+
+// pre increment
+// var num2 = ++num2
+//  console.log(num1, num2)
+
+
+
+// math ambiguity
+// var num1 = (5 + 6) * (2 + 3 -2) *4
+// console.log (num1);
+
+
+// CONCATENATION  (HAMESHA STRING ME HOTI HA)
+// var  text1 =  "hellow"
+// var text2 = "world"
+// console.log(text1 + text2)
+
+// var num1 = "55";
+// var num2 = 4;
+// console.log(num1+num2);
+
+
+// CONDITIONAL STATEMENT
+// if(condition) else 
+// var temp = 4;
+//  ===  value bi chexk karo aur type bi check karo
+// if( temp === 5 ) {
+//     console.log("take ine jacker along")
+// }else{
+//     console.log("you do not need to go outside")
+// }
