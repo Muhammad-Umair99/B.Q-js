1cfdb2a12da8ad60e148a116c57fbe4a34896ac2
Muhammad-Umair99 <muhammadumair10010@gmail.com>
2024-08-25 12:32:54 +0500

    if else & Array

---
+++ class2.js
@@ -0,0 +1,40 @@
+// else if 
+// var x = "Romeo"
+// var correctAnswer = "Vatican";
+// if (x === correctAnswer) {
+//     console.log("Correct");
+// }
+// else if (x === "Romeo") {
+//     console.log("Incorrect But close");
+// }
+// else{
+//     console.log("Incorrect")
+// }
+
+
+// && , or , !
+
+
+
+
+// array is a data type (civalized data type)
+
+// var student1 = "Umair"
+// var student2 = "Saad"
+// var student3 = "aqui" (not a good way to store data in different var)
+
+// array has a indexing value start from (zero to sooon)
+
+// var students = ["Umair", "Saad", "Muhammd","KHan"]
+// var student1 = students[0];
+// console.log(students[1]);
+
+// Array method
+// var students = ["Umair", "Saad", "Muhammd","KHan"]
+// console.log(students.pop(2));
+// students.push("Mudassir", "Yyyy");
+// console.log(students)
+
+// var students = ["Umair", "Saad", "Muhammd","KHan"]
+// students.splice(2,2, "Hammad" , "Amir", "Tanoli");
+// console.log(students);
