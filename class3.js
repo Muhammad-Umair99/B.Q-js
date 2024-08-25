4cb0bf6063f79d9c79db3d588c3f9fcc1f30a837
Muhammad-Umair99 <muhammadumair10010@gmail.com>
2024-08-25 12:35:48 +0500

    date & function

---
+++ class3.js
@@ -0,0 +1,48 @@
+// let date = new Date();()
+// console.log(date)
+
+
+// let r = new Date();
+// console.log( "date" , r.getDate());
+// console.log( "second", r.getSeconds());
+// console.log( "hour" , r.getHours());
+// console.log( "year", r.getFullYear());
+// console.log( "day" , r.getDay());
+// console.log( "month", r.getMonth());
+
+
+// let bornYear = prompt("enter your born year");
+// let rightNow = new Date;
+// let current = rightNow.getFullYear()
+// let age = current - bornYear;
+// console.log(" your age is", age);
+
+
+// function
+
+// function ageCalculator() {
+// let bornYear = 2002
+// let rightNow = new Date;
+// let current = rightNow.getFullYear()
+// let age = current - bornYear;
+// console.log(" your age is", age);
+
+// }
+// ageCalculator();
+
+// function tellTime(){
+//     let now = new Date();
+//     let theHr = now.getHours();
+//     let theMin = now.getMinutes();
+//     let theSec = now.getSeconds();
+//     console.log("curent time" ,theHr +  ":" + theMin , ":" + theSec);
+// }
+// tellTime()
+
+
+// function greeting (name){
+//     let greet = "Asalam O Alaikum" + " " + name;
+//     return greet
+// }
+// var a = greeting("Umair");
+// console.log(a);
